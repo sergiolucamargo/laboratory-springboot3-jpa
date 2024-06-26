4a36bed1cb132e0c559c5565cb05d035108945fe
sergiolucamargo <sergiolucamargo@gmail.com>
2024-06-25 22:08:40 -0300

    Product

---
+++ src/main/java/com/sergiocamargo/course/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.sergiocamargo.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sergiocamargo.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> { }
+
