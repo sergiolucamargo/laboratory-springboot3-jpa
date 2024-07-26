7955318cb7fdfc8ff019a4a444989675a4167ee8
sergiolucamargo <sergiolucamargo@gmail.com>
2024-07-25 21:50:30 -0300

    Exception handling - delete

--- src/main/java/com/sergiocamargo/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/sergiocamargo/course/resources/exceptions/ResourceExceptionHandler.java
@@ -23,4 +23,13 @@ public class ResourceExceptionHandler
 		return ResponseEntity.status(status).body(err);
 	}
 	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> dataBase(DatabaseException e, HttpServletRequest request)
+	{
+		String error = "Database Error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
 }
